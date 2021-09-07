72a50c0c5513627278828a1ee2ae124c13395e97
Adam <ad.kotlorz@gmail.com>
2021-09-07 22:00:38 +0200

    starting cfg

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+//Deps Imports
+import {StreamChat} from 'stream-chat';
+import {Chat} from 'stream-chat-react';
+//Components Imports
+import {ChannelContainer, ChannelListContainer} from './components';
+
+
+const apiKey = process.env.REACT_APP_API_KEY;
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
     return (
-      <div>
-
-      </div>  
+        <div className="app__wrapper">
+            <Chat client={client} theme="team light">
+                <ChannelListContainer/>
+                <ChannelContainer/>
+            </Chat>
+        </div>
     );
 }
 
